dad9510cf4a87e460b91d8a40c5ce93f90c0cfaa
pnavarroc <pnavarroc@iesch.org>
2026-02-17 19:32:50 +0100

    jhsd

--- Flutter chati/task_manager/lib/screens/marcador_screen.dart
+++ Flutter chati/task_manager/lib/screens/marcador_screen.dart
@@ -5,80 +5,57 @@ import 'package:task_manager/provider/marcador_provider.dart';
 class MarcadorScreen extends StatelessWidget {
   const MarcadorScreen({super.key});
 
-
-
-  
-
   @override
   Widget build(BuildContext context) {
-
     final marcador = context.watch<MarcadorProvider>();
 
     return Scaffold(
-      appBar: AppBar(title: Text("Marcador de fútbol"),),
+      appBar: AppBar(title: Text("Marcador de fútbol")),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Center(
           child: Column(
             children: [
-              Text("Marcador Visitante ",
-              style: TextStyle(
-                fontSize: 20,
+              Text("Marcador Visitante ", style: TextStyle(fontSize: 20)),
+              SizedBox(height: 10),
+              Text(
+                '${marcador.marcadorVisitante}',
+                style: TextStyle(fontSize: 60),
               ),
-              ),
-              SizedBox(height: 10,),
-              Text( '${marcador.marcadorVisitante}' ,
-              style: TextStyle(
-                fontSize: 60,
-              ),
-              ),
-              
               //Vamos a poner un icono Pero vamos a poner una row que tenga sumar 1 a visitante el icono y otro boton de sumar al local
               Expanded(
                 child: Row(
                   children: [
                     ElevatedButton(
-                      onPressed: ()=> marcador.incrementarVisitante(),
-                      child: Text("Gol visitantes")),
-                      SizedBox(width: 10,),
-                      Icon(
-                        Icons.sports_football,
-                        size: 100,
-                      ),
-                      SizedBox(width: 10,),
-                      ElevatedButton(
-                      onPressed: ()=> marcador.incrementarLocal(),
-                      child: Text("Gol local")),
-                      
+                      onPressed: () => marcador.incrementarVisitante(),
+                      child: Text("Gol visitantes"),
+                    ),
+                    SizedBox(width: 10),
+                    Icon(Icons.sports_football, size: 100),
+                    SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: () => marcador.incrementarLocal(),
+                      child: Text("Gol local"),
+                    ),
                   ],
                 ),
               ),
               ElevatedButton(
-                    onPressed: ()=> marcador.resetearMarcadores(),
-                    child: Text("Resetear")),
-              SizedBox(height: 100,),
-              Text("${marcador.marcadorLocal}",
-              style: TextStyle(
-                fontSize: 70,
+                onPressed: () => marcador.resetearMarcadores(),
+                child: Text("Resetear"),
               ),
-              ),
-              Text("Marcador Local ",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              ),
+              SizedBox(height: 100),
+              Text("${marcador.marcadorLocal}", style: TextStyle(fontSize: 70)),
+              Text("Marcador Local ", style: TextStyle(fontSize: 20)),
               ElevatedButton(
-                    onPressed: ()=> Navigator.pushNamed(context, 'resultado'),
-                    child: Text("Ir a Pantalla de resultados")),
-              SizedBox(height: 80,),
-
+                onPressed: () => Navigator.pushNamed(context, 'resultado'),
+                child: Text("Ir a Pantalla de resultados"),
+              ),
+              SizedBox(height: 80),
             ],
-            
           ),
         ),
       ),
     );
   }
 }
-
-
